e806f43e70078d88bed7161a41bc57dff587c26f
Molly Henry <henrymoll@myvuw.ac.nz>
2022-09-06 21:53:10 +1200

    Added Window
    window says "hello world"

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Main.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Main.java
@@ -1,7 +1,9 @@
 package nz.ac.vuw.ecs.swen225.gp22.app;
 
+import javax.swing.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world! - Chap's Challenge");
+    SwingUtilities.invokeLater(Start::new);
   }
 }
