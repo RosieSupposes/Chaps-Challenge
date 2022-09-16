fd4af881f872cfbb8c6e590df51213bf55beaae2
Wilkingide <wilkingide@ecs.vuw.ac.nz>
2022-09-17 00:06:59 +1200

    Initial commit. Initial structure for module and planning for xml file structure

---
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/persistency/ParseXML.java
@@ -0,0 +1,18 @@
+package nz.ac.vuw.ecs.swen225.gp22.persistency;
+
+/**
+ * Used to parse XML files.
+ *
+ * @author Gideon Wilkins, 300576057
+ * @version 1.1
+ */
+public class ParseXML {
+    /**
+     * Load a level from file
+     *
+     * @param level level name/number to load
+     */
+    public static void loadLevel(String level){
+        System.out.println("Loading level from: " + level + ".xml");
+    }
+}
