731fdfbbd0f6ff63b3167956ed154eab608b0431
Molly Henry <henrymoll@myvuw.ac.nz>
2022-09-18 12:32:21 +1200

    Allow recorder to return to menu
    Make startPhase() public to allow recorder to exit to menu from player window.

--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Base.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/Base.java
@@ -16,27 +16,12 @@ import java.util.List;
  * @version 1.3
  */
 public class Base extends JFrame {
-    private Runnable closePhase = () -> {
-        for (JComponent component : this.components) {
-            remove(component);
-        }
-    };
-    public final int TILE_SIZE = 60;
-    public final int NUM_GAME_TILE = 9;
-    public final int SIDEBAR_WIDTH = 5 * TILE_SIZE;
-    public final int WINDOW_HEIGHT = NUM_GAME_TILE * TILE_SIZE;
-    public final int GAME_WINDOW_SIZE = WINDOW_HEIGHT;
-    public final int WINDOW_WIDTH = NUM_GAME_TILE * TILE_SIZE + SIDEBAR_WIDTH;
-
-    private List<JComponent> components = new ArrayList<>();
-
+    private final List<JComponent> components = new ArrayList<>();
     public final Runnable startAction = () -> levelPhase(true);
-    public final Runnable pauseAction = () -> {
-        System.out.println("Pause");
-    };
-    public final Runnable unPauseAction = () -> {
-        System.out.println("Un Pause");
-    };
+    private int timeMS = 0;
+    private int timeSec = 0;
+    private Timer timer = new Timer(20,null);
+
     public final Runnable loadAction = () -> {
         System.out.println("Load");
     };
@@ -57,26 +42,34 @@ public class Base extends JFrame {
     };
     public final Runnable exitAction = () -> {
         System.out.println("Exit");
-        closePhase.run();
+        runClosePhase();
         System.exit(0);
     };
-    private int timeMS = 0;
-    private int timeSec = 0;
-    private Timer timer = new Timer(20,null);
 
+    public final Runnable pauseAction = () -> {
+        System.out.println("Pause");
+        pausePhase();
+    };
+
+    public final Runnable unPauseAction = () -> {
+        System.out.println("Un Pause");
+        unPause();
+    };
 
     Base() {
         assert SwingUtilities.isEventDispatchThread();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
         startPhase();
+
         setVisible(true);
         setResizable(false);
-        setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+        setPreferredSize(new Dimension(Main.WINDOW_WIDTH, Main.WINDOW_HEIGHT));
         pack();
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosed(WindowEvent e) {
-                closePhase.run();
+                runClosePhase();
             }
         });
     }
@@ -86,7 +79,9 @@ public class Base extends JFrame {
     }
 
     public void runClosePhase() {
-        closePhase.run();
+        for (JComponent component : this.components) {
+            remove(component);
+        }
         components.clear();
         timer.stop();
     }
@@ -95,12 +90,16 @@ public class Base extends JFrame {
         components.add(jc);
     }
 
-    private void startPhase() {
+    public void startPhase() {
+        runClosePhase();
+        setJMenuBar(null);
+
         PhasePanel menu = new PhasePanel(new MenuMainPanel(this),new MenuSidePanel());
-        //Phase menu = new Phase(this);
         add(BorderLayout.CENTER, menu);
         components.add(menu);
         components.addAll(menu.getAllComponents());
+
+        pack();
     }
 
     public void levelPhase(boolean isLevelOne) {
@@ -118,13 +117,14 @@ public class Base extends JFrame {
             level = new PhasePanel(game, side);
         } else {
             JPanel game = new JPanel();
-            game.setBackground(Color.RED);
+            game.setBackground(Color.ORANGE);
 
             JPanel side = new JPanel();
-            side.setBackground(Color.CYAN);
+            side.setBackground(Color.PINK);
 
             level = new PhasePanel(game, side);
         }
+
         PhasePanel finalLevel = level;
         finalLevel.addKeyListener(new Controller(this));
         finalLevel.setFocusable(true);
@@ -138,25 +138,33 @@ public class Base extends JFrame {
                 System.out.println(timeSec++);
             }
         });
-
-        runClosePhase();
-
-        add(BorderLayout.CENTER, finalLevel);//add the new phase viewport
-        setJMenuBar(new GameMenuBar(this));
-        setPreferredSize(getSize());//to keep the current size
-        pack();                     //after pack
-        finalLevel.requestFocus();//need to be after pack
         timer.start();
 
+        add(BorderLayout.CENTER, finalLevel);//add the new phase viewport
         components.add(finalLevel);
+        GameMenuBar gameMenuBar = new GameMenuBar(this);
+        gameMenuBar.addGameButtons();
+        gameMenuBar.addLoadButton();
+        gameMenuBar.addExitButton();
+        setJMenuBar(gameMenuBar);
+
+        pack();                     //after pack
+        finalLevel.requestFocus();  //need to be after pack
     }
 
     private void pausePhase() {
+        //runClosePhase();
+        timer.stop();
+        //pack();
+    }
 
+    private void unPause(){
+        timer.start();
     }
 
     private void replayPhase() {
         runClosePhase();
+
         Player playerWindow = new Player();
         add(BorderLayout.CENTER, playerWindow);//add the new phase viewport
         setPreferredSize(getSize());//to keep the current size
--- src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/GameMenuBar.java
+++ src/main/java/nz/ac/vuw/ecs/swen225/gp22/app/GameMenuBar.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class GameMenuBar extends JMenuBar {
 
     List<JComponent> components = new ArrayList<>();
+    int menuItemWidth;
     Base base;
 
     public GameMenuBar(Base base){
@@ -16,8 +17,23 @@ public class GameMenuBar extends JMenuBar {
     }
 
     public void setMenuBarForGame() {
-        int menuItemWidth = Main.GAME_WINDOW_SIZE / 5;
+        menuItemWidth = Main.GAME_WINDOW_SIZE / 6;
+        addHomeButton();
+    }
 
+    private void addHomeButton(){
+        JMenuItem home = new JMenuItem("Home");
+        home.setMaximumSize(new Dimension(menuItemWidth, 30));
+        home.addActionListener(e -> base.startPhase());
+        this.add(home);
+        components.add(home);
+    }
+
+    /**
+     * Adds buttons to the menu bar that are only relevant to the game window.
+     * Pause, New - level 1, level 2, Save
+     */
+    public void addGameButtons(){
         JMenuItem pause = new JMenuItem("Pause");
         pause.setMaximumSize(new Dimension(menuItemWidth, 30));
         pause.addActionListener(e -> base.pauseAction.run());
@@ -45,13 +61,17 @@ public class GameMenuBar extends JMenuBar {
         save.addActionListener(e -> base.saveAction.run());
         this.add(save);
         components.add(save);
+    }
 
+    public void addLoadButton(){
         JMenuItem load = new JMenuItem("Load");
         load.setMaximumSize(new Dimension(menuItemWidth, 30));
         load.addActionListener(e -> base.loadAction.run());
         this.add(load);
         components.add(load);
+    }
 
+    public void addExitButton(){
         JMenuItem exit = new JMenuItem("Exit");
         exit.setMaximumSize(new Dimension(menuItemWidth, 30));
         exit.addActionListener(e -> base.exitAction.run());
