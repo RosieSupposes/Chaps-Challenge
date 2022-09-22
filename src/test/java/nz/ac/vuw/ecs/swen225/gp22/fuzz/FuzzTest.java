341d77fa7184598af27eac5d05b94c43a783367e
Gavin L <limgavi@ecs.vuw.ac.nz>
2022-09-22 15:22:51 +1200

    Generates and inputs random movement

--- src/test/java/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
+++ src/test/java/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
@@ -4,6 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 import java.time.Duration;
 import org.junit.jupiter.api.Test;
+import java.util.*;
+import java.util.stream.IntStream;
+import javax.swing.SwingUtilities;
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+import nz.ac.vuw.ecs.swen225.gp22.app.*;
 
 /**
  * Class for Fuzz Testing
@@ -13,12 +20,82 @@ import org.junit.jupiter.api.Test;
  */
 public class FuzzTest {
 
+    static final Random r = new Random();
+
+    // List of possible inputs
+    List<Integer> inputs = List.of(KeyEvent.VK_UP,
+            KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT, KeyEvent.VK_DOWN);
+
+    private List<Integer> generateInputs() {
+        return IntStream.range(0, 100)
+                .map(i -> r.nextInt(inputs.size()))
+                .mapToObj(j -> inputs.get(j))
+                .toList();
+    }
+
+    private int generateOneInput() {
+        return inputs.get(r.nextInt(inputs.size()));
+    }
+
     public void test1() {
-            
+        try {
+            Robot robot = new Robot();
+            try {
+                SwingUtilities.invokeLater(() -> new Base().levelPhase(true));
+            } catch (Error e) {
+            }
+            List<Integer> generatedInputs = generateInputs();
+            for (int i : generatedInputs) {
+                if (SwingUtilities.isEventDispatchThread()) {
+
+                } else {
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            robot.keyPress(generateOneInput());
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
+                    SwingUtilities.invokeLater(() -> robot.keyPress(generatedInputs.get(i)));
+                }
+            }
+
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
     }
 
     public void test2() {
-        
+        try {
+            Robot robot = new Robot();
+            try {
+                SwingUtilities.invokeLater(() -> new Base().levelPhase(true));
+            } catch (Error e) {
+
+            }
+            List<Integer> generatedInputs = generateInputs();
+            for (int i : generatedInputs) {
+                if (SwingUtilities.isEventDispatchThread()) {
+
+                } else {
+                    SwingUtilities.invokeLater(new Runnable() {
+                        public void run() {
+                            robot.keyPress(generateOneInput());
+                        }
+                    });
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                    }
+                    SwingUtilities.invokeLater(() -> robot.keyPress(generatedInputs.get(i)));
+                }
+            }
+
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
