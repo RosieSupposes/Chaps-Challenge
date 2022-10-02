88ea905bcc485409b5f61165a36c7a5daaa12bda
Molly Henry <henrymoll@ecs.vuw.ac.nz>
2022-10-02 22:25:15 +1300

    Give Fuzz board size

--- src/main/nz/ac/vuw/ecs/swen225/gp22/app/Base.java
+++ src/main/nz/ac/vuw/ecs/swen225/gp22/app/Base.java
@@ -180,6 +180,10 @@ public class Base extends JFrame {
         System.exit(0);
     }
 
+    public Maze.Point getBoardSize(){
+        return Maze.getDimensions();
+    }
+
     /**
      * When key is pressed, move player and tell recorder
      *
