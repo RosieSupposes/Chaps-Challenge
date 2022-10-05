bb7a9c0f9ca6375a93401f2640aebdedc6f016cc
Diana Batoon <batoondian@ecs.vuw.ac.nz>
2022-10-05 22:22:17 +1300

    Modified Viewport class

--- src/main/nz/ac/vuw/ecs/swen225/gp22/renderer/Viewport.java
+++ src/main/nz/ac/vuw/ecs/swen225/gp22/renderer/Viewport.java
@@ -3,8 +3,7 @@ package nz.ac.vuw.ecs.swen225.gp22.renderer;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,205 +19,115 @@ import nz.ac.vuw.ecs.swen225.gp22.domain.*;
  * @version 1.4
  */
 public class Viewport extends JPanel implements ActionListener {
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  private final Tile[][] currentMaze;
-  private final Tile[][] previousMaze; // the view of the maze before a player moves
+    private Tile[][] currentMaze = new Tile[GameConstants.NUM_GAME_TILE][GameConstants.NUM_GAME_TILE];
+    private Timer timer;
 
-  /**
-   * Initialises a new maze upon the loading of a level.
-   */
-  public Viewport(){ 
-    currentMaze = new Tile[GameConstants.NUM_GAME_TILE][GameConstants.NUM_GAME_TILE];
-    previousMaze = new Tile[GameConstants.NUM_GAME_TILE][GameConstants.NUM_GAME_TILE]; 
-  }
-
-  /**
-   * Displays all the tiles in the focus area.
-   *
-   * @param g Graphics object needed to render images on the canvas.
-   * @param xOffset Offset in the X direction.
-   * @param yOffset Offset in the Y direction.
-   * @param maze The maze to be drawn on. 
-   */
-  public void renderTiles(Graphics g, int xOffset, int yOffset, Tile[][] maze){
-    for (int x = 0; x < GameConstants.NUM_GAME_TILE; x++){
-        for (int y = 0; y < GameConstants.NUM_GAME_TILE; y++){
-            if (maze[x][y] != null) { // check that the tile is not null
-                g.drawImage(getTileImg(Maze.getTile(new Maze.Point(x,y))), 
-                (x*GameConstants.TILE_SIZE) + xOffset, (y*GameConstants.TILE_SIZE) + yOffset, this);
-            } 
+    /**
+     * Initialises a new maze upon the loading of a level.
+     */
+    public Viewport(){ 
+        for (int x = 0 ; x < GameConstants.NUM_GAME_TILE; x++){
+            for (int y = 0; y < GameConstants.NUM_GAME_TILE; y++){
+                currentMaze[x][y] = Maze.getTile(new Maze.Point(x, y));
+            }
         }
+
+        timer = new Timer(300, this);
+        timer.start();
     }
-  }
-
-/**
- * Returns a BufferedImage corresponding to the type of tile in the maze.
- * 
- * @param tile The tile being drawn on the canvas.
-*/
-  public BufferedImage getTileImg(Tile tile){
-    if (tile instanceof Key k){ // check the colours against the four different ones available
-      switch (k.getColor()){
-        case Blue: return Img.BlueKey.image;
-        case Green: return Img.GreenKey.image;
-        case Red: return Img.RedKey.image;
-        case Yellow: return Img.YellowKey.image;
-        default: throw new IllegalArgumentException("Invalid colour./n");
-      }
-    }            
-    if (tile instanceof LockedDoor ld){
-        switch (ld.getColor()){
-          case Blue: return Img.BlueLockedDoor.image;
-          case Green: return Img.GreenLockedDoor.image;
-          case Red: return Img.RedLockedDoor.image;
-          case Yellow: return Img.YellowLockedDoor.image;
-          default: throw new IllegalArgumentException("Invalid colour./n");
-        }
-    }
-    if (tile instanceof InfoField){ return Img.InfoField.image; } 
-    if (tile instanceof Exit){ return Img.Exit.image; } 
-    if (tile instanceof LockedExit){ return Img.LockedExit.image; }
-    if (tile instanceof Treasure){ return Img.Treasure.image; }
-    if (tile instanceof Wall){ return Img.Wall.image; }
-
-    return Img.FreeTile.image;
-  }
-
-  /**
-   * When the player is stepping on the InfoField, 
-   * the information about the game that the player 
-   * needs to do in order to complete a level will be displayed.
-   * 
-   * @param g Graphics object needed to render images on the canvas.
-  */
-  public void displayInfo(Tile t, Graphics g){
-    g.drawImage(Img.GameInfo.image, 100, 200, this); 
-  }
-
-  /**
-   * Displays all the entities moving on the tiles.
-   * Updates the position and image of each entity that moves.
-   *
-   * @param g Graphics object needed to render images on the canvas.
-   * @param xOffset Offset in the X direction.
-   * @param yOffset Offset in the Y direction.
-   */
-  public void renderEntities(Graphics g, int xOffset, int yOffset){
-    // draws the player based on the direction it is facing
-    g.drawImage(getEntityImg(Maze.player.getDir(), true), getFocusX(Maze.player.getPos().x()*GameConstants.TILE_SIZE),
-    getFocusY(Maze.player.getPos().y()*GameConstants.TILE_SIZE), this);
     
-    //TODO: display the enemy for level 2
-    for (Entity enemy: Maze.entities){
-      g.drawImage(Img.EnemyRight.image, enemy.getPos().x()*GameConstants.TILE_SIZE, enemy.getPos().y()*GameConstants.TILE_SIZE, this);
-    }
-  }
+    public void paint(Graphics g){
+        Graphics2D g2D = (Graphics2D)g.create();
+        for (int x = 0; x < GameConstants.NUM_GAME_TILE; x++){
+            for (int y = 0; y < GameConstants.NUM_GAME_TILE; y++){
+                // draw the tiles
+                g2D.drawImage(getTileImg(Maze.getTile(new Maze.Point(x, y))), 
+                x * GameConstants.TILE_SIZE,
+                y * GameConstants.TILE_SIZE, this);
 
-  /**
-   * Returns a BufferedImage corresponding to the direction 
-   * an entity is facing. 
-   * 
-   * @param dir The direction the entity is facing.
-   * @param player Whether the entity is a player.
-   * @return The image of an entity depending on the direction it is facing.
-   */
-  public BufferedImage getEntityImg(Entity.Direction dir, boolean player){
-    if (player){
-      // check for the facingDir of player then return image accordingly
-      switch (dir){
-        case Up: return Img.PlayerUp.image;
-        case Down: return Img.PlayerDown.image;
-        case Left: return Img.PlayerLeft.image; 
-        case Right: return Img.PlayerRight.image;
-        default: throw new IllegalArgumentException("Invalid direction./n");
-      }
-    }
-    // check for the facingDir of the enemy for level 2
-    switch (dir){
-      case Up: return Img.EnemyUp.image;
-      case Down: return Img.EnemyDown.image;
-      case Left: return Img.EnemyLeft.image; 
-      case Right: return Img.EnemyRight.image;
-      default: throw new IllegalArgumentException("Invalid direction./n");
-    }
-  }
-
-  /*
-  * Renders a new maze that focuses on the tiles within 
-  * the vicinity of the player's current location on the canvas. 
-  */
-  public void setFocusArea(){
-    // loop through each tile in the maze to determine which are visible
-    for (int xTick = 0, x = Maze.player.getPos().x() - GameConstants.FOCUS_AREA; x <= Maze.player.getPos().x() + GameConstants.FOCUS_AREA; xTick++, x++){
-      for (int yTick = 0, y = Maze.player.getPos().y() - GameConstants.FOCUS_AREA; y <= Maze.player.getPos().y() + GameConstants.FOCUS_AREA; yTick++, y++){
-        previousMaze[xTick][yTick] = currentMaze[xTick][yTick];
-        
-        // Check if tiles are within range
-        if ((x > 0 && y > 0) && (x < currentMaze.length && y < currentMaze[x].length)) {
-          currentMaze[xTick][yTick] = Maze.getTile(new Maze.Point(x, y));
+                // draw the player
+                g2D.drawImage(getEntityImg(Maze.player.getDir()), 
+                Maze.player.getPos().x() * GameConstants.TILE_SIZE,
+                Maze.player.getPos().y() * GameConstants.TILE_SIZE, this);
+            }
         }
-      }
     }
-  }
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    Tile[][] temp = new Tile[GameConstants.NUM_GAME_TILE][GameConstants.NUM_GAME_TILE];
-    for (int x =0; x < temp.length; x++){
-      for(int y =0; y < temp.length; y++){
-        if (Maze.player instanceof Player){
-          setFocusArea(x,y, temp);
+    /*** Updates every 100 ms */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //TODO: only works when the timer starts
+        //Tile[][] tempMaze = new Tile[Maze.getDimensions().x()][Maze.getDimensions().y()];
+        Tile[][] tempMaze = new Tile[GameConstants.NUM_GAME_TILE][GameConstants.NUM_GAME_TILE];
+        int playerX = Maze.player.getPos().x();
+        int playerY = Maze.player.getPos().y();
+
+        //System.out.println("Action Performed.");
+
+        for(int x = 0; x < tempMaze.length; x++) {
+            for(int y = 0; y < tempMaze[x].length; y++) {
+                //System.out.println("Second for loop.");
+                // show only the tiles in the player's vicinity
+                if (x == playerX && y == playerY){
+                    setFocusArea(x,y,tempMaze); //TODO: throws an error
+                    //System.out.println("Reached focus area.");
+                }
+            }
         }
-      }
+        repaint();
     }
-    repaint();
-    updateUI();
-  }
 
-  public void setFocusArea(int x, int y, Tile[][] temp){
-    int focusX = x-GameConstants.FOCUS_AREA;
-    int focusY = y-GameConstants.FOCUS_AREA;
+    private void setFocusArea(int x, int y, Tile[][] tempMaze) {
+        int focusX = x - GameConstants.FOCUS_AREA; 
+        int focusY = y - GameConstants.FOCUS_AREA;
 
-    for (int i = 0; i < GameConstants.NUM_GAME_TILE; i++){
-      for (int j = 0; j < GameConstants.NUM_GAME_TILE; j++){
-        currentMaze[i][j] = temp[i+focusX][j+focusY];
-      }
-
+        for(int i = 0; i < GameConstants.NUM_GAME_TILE; i++) {
+            for(int j = 0; j < GameConstants.NUM_GAME_TILE; j++) {
+                currentMaze[i][j] = tempMaze[i + focusX][j+ focusY];
+                //System.out.println("Update focus area.");
+            }
+        }
     }
-  }
 
+    public BufferedImage getTileImg(Tile tile){
+        if (tile instanceof Key k){ // check the colours against the four different ones available
+            switch (k.getColor()){
+                case Blue: return Img.BlueKey.image;
+                case Green: return Img.GreenKey.image;
+                case Red: return Img.RedKey.image;
+                case Yellow: return Img.YellowKey.image;
+                default: throw new IllegalArgumentException("Invalid colour./n");
+            }
+        }            
+        if (tile instanceof LockedDoor ld){
+            switch (ld.getColor()){
+                case Blue: return Img.BlueLockedDoor.image;
+                case Green: return Img.GreenLockedDoor.image;
+                case Red: return Img.RedLockedDoor.image;
+                case Yellow: return Img.YellowLockedDoor.image;
+                default: throw new IllegalArgumentException("Invalid colour./n");
+            }
+        }
+        if (tile instanceof InfoField){ return Img.InfoField.image; } 
+        if (tile instanceof Exit){ return Img.Exit.image; } 
+        if (tile instanceof LockedExit){ return Img.LockedExit.image; }
+        if (tile instanceof Treasure){ return Img.Treasure.image; }
+        if (tile instanceof Wall){ return Img.Wall.image; }
 
-  /**
-   * Convert the player's x position to the focus area's coordinates.
-   *
-   * @param x The player's x position.
-   * @return The x position in the focus area.
-   */
-  public int getFocusX(int x) { return GameConstants.FOCUS_AREA + x - Maze.player.getPos().x() ; }
-  
-  /**
-   * Convert the player's y position to the focus area's coordinates.
-   *
-   * @param y The player's y position.
-   * @return The y position in the focus area.
-   */
-  public int getFocusY(int y) { return y - Maze.player.getPos().y() + GameConstants.FOCUS_AREA; }
+        return Img.FreeTile.image;
+    }
 
-  @Override
-  public void paintComponent(Graphics g){
-    super.paintComponent(g);
-    Graphics2D g2D = (Graphics2D) g.create();
-    int xOffset = 0;
-    int yOffset = 0;
-
-    setFocusArea(); // TODO: replace with new focusarea
-    renderTiles(g2D, xOffset, yOffset, currentMaze); //TODO: make offset values change depending on the frame
-    renderEntities(g, xOffset, yOffset);
-    
-    //g2D.dispose(); // releases the graphics object
-  }
+    public BufferedImage getEntityImg(Entity.Direction dir){
+        // check for the facingDir of player then return image accordingly
+        switch (dir){
+            case Up: return Img.PlayerUp.image;
+            case Down: return Img.PlayerDown.image;
+            case Left: return Img.PlayerLeft.image; 
+            case Right: return Img.PlayerRight.image;
+            default: throw new IllegalArgumentException("Invalid direction./n");
+        }
+    }
 
 }
-
-  
