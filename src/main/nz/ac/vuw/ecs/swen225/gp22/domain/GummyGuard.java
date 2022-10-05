b8ae7972205b6c8b283b77ca0eaddf1ce805602d
asfariabdu <asfariabdu@vuw.ac.nz>
2022-10-05 21:31:07 +1300

    Added gummy enemy class

---
+++ src/main/nz/ac/vuw/ecs/swen225/gp22/domain/GummyGuard.java
@@ -0,0 +1,38 @@
+package nz.ac.vuw.ecs.swen225.gp22.domain;
+
+/**
+ * A basic enemy that INSERT BEHAVIOR HERE.
+ * 
+ * @author Abdulrahman Asfari, 300475089
+ * @version 1.1
+ */
+public class GummyGuard extends Entity<GummyGuard>{
+    /** Used for if the player walks into the enemy. */
+    private final Observer<Player> playerObserver;
+
+    /**
+     * Default constructor, sets the position and direction of the enemy.
+     * 
+     * @param entityPos {@link Maze.Point Point} to set the position field to. ({@link Entity#entityPos see here})
+     * @param facingDir {@link Direction} to set the direction field to. ({@link Entity#facingDir see here})
+     */
+    public GummyGuard(Maze.Point entityPos, Direction facingDir){
+        super(entityPos, facingDir);
+
+        playerObserver = player -> {
+            if(player.getPos().equals(entityPos)){
+                // DEATH AND TAXES
+                System.out.println("Goodbye, I have died.");
+            }
+        };
+        Maze.player.addObserver(playerObserver);
+    }
+
+    @Override
+    public Action ping(){
+        return moveAndTurn(getDir());
+    }
+
+    @Override
+    public void deleteEntity(){ Maze.player.removeObserver(playerObserver); }
+}
