499a74c3ec2c356c6484cc4bfae5d7915725f10d
asfariabdu <asfariabdu@vuw.ac.nz>
2022-10-05 21:36:56 +1300

    Added base enemy class

--- src/main/nz/ac/vuw/ecs/swen225/gp22/domain/GummyGuard.java
+++ src/main/nz/ac/vuw/ecs/swen225/gp22/domain/GummyGuard.java
@@ -4,20 +4,21 @@ package nz.ac.vuw.ecs.swen225.gp22.domain;
  * A basic enemy that INSERT BEHAVIOR HERE.
  * 
  * @author Abdulrahman Asfari, 300475089
- * @version 1.1
+ * @version 1.2
  */
-public class GummyGuard extends Entity<GummyGuard>{
+public class GummyGuard extends EnemyEntity<GummyGuard>{
     /** Used for if the player walks into the enemy. */
     private final Observer<Player> playerObserver;
 
     /**
-     * Default constructor, sets the position and direction of the enemy.
+     * Default constructor, sets the position, direction, and speed of the enemy.
      * 
      * @param entityPos {@link Maze.Point Point} to set the position field to. ({@link Entity#entityPos see here})
      * @param facingDir {@link Direction} to set the direction field to. ({@link Entity#facingDir see here})
+     * @param speed How often the monster gets pinged. 
      */
     public GummyGuard(Maze.Point entityPos, Direction facingDir){
-        super(entityPos, facingDir);
+        super(entityPos, facingDir, 400);
 
         playerObserver = player -> {
             if(player.getPos().equals(entityPos)){
