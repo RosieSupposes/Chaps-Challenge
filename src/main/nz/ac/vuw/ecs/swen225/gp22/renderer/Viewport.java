e092904b407c5ac339ec5d39856e0a7ffd0d912b
Diana Lindsay Batoon <batoondian@ecs.vuw.ac.nz>
2022-10-06 19:26:00 +1300

    Removed unused import.

--- src/main/nz/ac/vuw/ecs/swen225/gp22/renderer/Viewport.java
+++ src/main/nz/ac/vuw/ecs/swen225/gp22/renderer/Viewport.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import javax.swing.Timer;
-import javax.swing.plaf.FontUIResource;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
