f024c7d6f33ed10ccfdf00f6bab39228949c8acf
Gavin L <limgavi@ecs.vuw.ac.nz>
2022-10-11 02:10:06 +1300

    Refactored and individual tests close window on finish.

--- src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
@@ -13,20 +13,20 @@ import java.awt.AWTException;
 import java.awt.event.KeyEvent;
 
 /**
- * Class for Fuzz Testing
- * 
+ * Class for Fuzz Testing.
+ *
  * @author Gavin Lim, 300585341
- * @version 1.3
+ * @version 1.4
  */
 public class FuzzTest {
 
     static final Random r = new Random();
 
-    // List of possible inputs
+    // List of possible inputs.
     private final List<Integer> inputs = List.of(KeyEvent.VK_UP,
             KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT, KeyEvent.VK_DOWN);
 
-    //Map of inputs and their opposite inputs
+    //Map of inputs and their opposite inputs.
     private final Map<Integer, Integer> inputsAndOpposite =
             Map.of(KeyEvent.VK_UP, KeyEvent.VK_DOWN,
                     KeyEvent.VK_DOWN, KeyEvent.VK_UP,
@@ -34,10 +34,10 @@ public class FuzzTest {
                     KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT);
 
     /**
-     * Generates a list of 500 valid inputs
-     * Chap will not move back to the tile he was previously on
-     * 
-     * @return list of 500 valid inputs
+     * Generates a list of 500 valid inputs.
+     * Chap will not move back to the tile he was previously on.
+     *
+     * @return list of 500 valid inputs.
      */
     private List<Integer> generateInputs() {
         int prev = -1;
@@ -57,56 +57,38 @@ public class FuzzTest {
     }
 
     /**
-     * Generates a single valid input
-     * 
-     * @return single valid input
+     * Generates a single valid input.
+     *
+     * @return single valid input.
      */
     private int generateOneInput() {
         return inputs.get(r.nextInt(inputs.size()));
     }
 
     /**
-     * Inputs random inputs for Level 1 of Chap's Challenge
+     * Inputs random inputs for Level 1 of Chap's Challenge.
      */
     public void test1() {
-        try {
-            Robot robot = new Robot();
-            try {
-                SwingUtilities.invokeLater(() -> new Base().newGame(1));
-            } catch (Error e) {
-            }
-            List<Integer> generatedInputs = generateInputs();
-            for (int i : generatedInputs) {
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        robot.keyPress(i);
-                    }
-                });
-                try {
-                    Thread.sleep(100);
-                    SwingUtilities.invokeLater(new Runnable() {
-                        public void run() {
-                            robot.keyRelease(i);
-                        }
-                    });
-                } catch (InterruptedException e) {
-                }
-            }
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
+        runTest(1);
     }
 
     /**
-     * Inputs random inputs for Level 2 of Chap's Challenge
+     * Inputs random inputs for Level 2 of Chap's Challenge.
      */
     public void test2() {
+        runTest(2);
+    }
+
+    /**
+     * Runs the test for the specified level.
+     * @param level - level number.
+     */
+    private void runTest(int level) {
         try {
             Robot robot = new Robot();
             try {
-                SwingUtilities.invokeLater(() -> new Base().newGame(1));
+                SwingUtilities.invokeLater(() -> new Base().newGame(level));
             } catch (Error e) {
-
             }
             List<Integer> generatedInputs = generateInputs();
             for (int i : generatedInputs) {
@@ -125,18 +107,36 @@ public class FuzzTest {
                 } catch (InterruptedException e) {
                 }
             }
+            try {
+                robot.keyPress(KeyEvent.VK_CONTROL);
+                Thread.sleep(100);
+                robot.keyPress(KeyEvent.VK_X);
+                Thread.sleep(100);
+                robot.keyRelease(KeyEvent.VK_X);
+                Thread.sleep(100);
+                robot.keyRelease(KeyEvent.VK_CONTROL);
+            }
+            catch(Exception e) {
+
+            }
         } catch (AWTException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * Runs tests for both level 1 and 2 of Chap's Challenge
-     * Tests are limited to a one minute timer
+     * Runs tests for both level 1 and 2 of Chap's Challenge.
+     * Tests are limited to a one minute timer.
      */
     @Test
     public void fuzzTests() {
-        assertTimeout(Duration.ofSeconds(60), () -> test1());
-        assertTimeout(Duration.ofSeconds(60), () -> test2());
+        try {
+            assertTimeout(Duration.ofSeconds(60), () -> test1());
+            Thread.sleep(5000);
+            assertTimeout(Duration.ofSeconds(60), () -> test2());
+        }
+        catch(Exception e) {
+
+        }
     }
 }
