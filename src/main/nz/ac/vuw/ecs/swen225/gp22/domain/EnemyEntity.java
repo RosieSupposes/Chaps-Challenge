f66eba9fdda8450c3af07212105361e64c1aa2d2
asfariabdu <asfariabdu@vuw.ac.nz>
2022-10-12 17:38:10 +1300

    Added imageMap to EnemyEntity

--- src/main/nz/ac/vuw/ecs/swen225/gp22/domain/EnemyEntity.java
+++ src/main/nz/ac/vuw/ecs/swen225/gp22/domain/EnemyEntity.java
@@ -1,17 +1,22 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain;
 
 import nz.ac.vuw.ecs.swen225.gp22.domain.Maze.Point;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
 
 /**
  * Base class for enemies, has a speed property so app
  * knows how often to ping the enemy.
  * 
  * @author Abdulrahman Asfari, 300475089
- * @version 1.1
+ * @version 1.2
  */
 public abstract class EnemyEntity<S extends Observable<S>> extends Entity<S>{
     /** How often the monster gets pinged. */
     private final int speed;
+
+    /** Map used by renderer to get image based on direction. */
+    public static HashMap<Direction, BufferedImage> imageMap = new HashMap<>();
 
     /**
      * Default constructor, sets the position, direction, and speed of the enemy.
