74bcec0d0ab82af0e03555f0b1c599f742fcadab
asfariabdu <asfariabdu@vuw.ac.nz>
2022-10-13 23:59:03 +1300

    Reformatted domain tests

--- src/test/nz/ac/vuw/ecs/swen225/gp22/domain/DomainTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/domain/DomainTest.java
@@ -7,37 +7,37 @@ import org.junit.jupiter.api.Test;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Entity.Direction;
 
 /**
- * Tests to check the integrity of the game 
- * state in the domain module. 
- * 
- * These tests cover everything except for the DevMarkers class, 
+ * Tests to check the integrity of the game
+ * state in the domain module.
+ * <p>
+ * These tests cover everything except for the DevMarkers class,
  * which is used for testing and marking work in progress code.
- * 
+ *
  * @author Abdulrahman Asfari, 300475089
  * @version 1.2
  */
-public class DomainTest{
+public class DomainTest {
     /**
      * Checks that a new maze is created properly.
      */
     @Test
-    public void newMazeTest(){
+    public void newMazeTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
 
         assert Maze.player.getPos().equals(new Maze.Point(0, 0));
         assert Maze.getDimensions().equals(new Maze.Point(5, 3));
         assert Maze.getStringState().equals(
-            "GGGGG\n" +
-            "GGGGG\n" + 
-            "GGGGG\n"
+                "GGGGG\n" +
+                        "GGGGG\n" +
+                        "GGGGG\n"
         );
-    } 
+    }
 
     /**
      * Checks that all tiles can be added properly.
      */
     @Test
-    public void createTilesTest(){
+    public void createTilesTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         Maze.setTile(new Maze.Point(0, 0), new Ground(new Maze.Point(0, 0)));
         Maze.setTile(new Maze.Point(1, 0), new InfoField(new Maze.Point(1, 0), "Hello"));
@@ -51,17 +51,17 @@ public class DomainTest{
         Maze.setTile(new Maze.Point(4, 1), new BouncyPad(new Maze.Point(4, 1), Direction.Left));
 
         assert Maze.getStringState().equals(
-            "GIKDL\n" +
-            "TWEMB\n" + 
-            "GGGGG\n"
+                "GIKDL\n" +
+                        "TWEMB\n" +
+                        "GGGGG\n"
         );
-    } 
+    }
 
     /**
      * Checks that the player can move properly.
      */
     @Test
-    public void movePlayerTest(){
+    public void movePlayerTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
 
         assert Maze.player.id() == 0;
@@ -89,7 +89,7 @@ public class DomainTest{
      * Checks that created tiles have the expected properties.
      */
     @Test
-    public void correctTileInfoTest(){
+    public void correctTileInfoTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         InfoField infoTile = new InfoField(new Maze.Point(1, 0), "Hello");
         Key key = new Key(new Maze.Point(2, 0), ColorableTile.Color.Blue);
@@ -104,7 +104,7 @@ public class DomainTest{
      * Checks that the player can correctly pickup a key.
      */
     @Test
-    public void keyPickupTest(){
+    public void keyPickupTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         Maze.setTile(new Maze.Point(2, 0), new Key(new Maze.Point(2, 0), ColorableTile.Color.Blue));
 
@@ -116,7 +116,7 @@ public class DomainTest{
         assert Maze.player.getAllKeys().size() == 1;
         assert Maze.player.hasKey(ColorableTile.Color.Blue);
         Maze.player.resetItems();
-        assert Maze.player.keyCount() == 0; 
+        assert Maze.player.keyCount() == 0;
         assert Maze.player.getPos().equals(new Maze.Point(2, 0));
     }
 
@@ -125,14 +125,17 @@ public class DomainTest{
      * only when the player has the correct key, and that the key is consumed.
      */
     @Test
-    public void unlockDoorTest(){
+    public void unlockDoorTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         LockedDoor door = new LockedDoor(new Maze.Point(1, 0), ColorableTile.Color.Blue);
         Maze.setTile(new Maze.Point(1, 0), door);
         Maze.setTile(new Maze.Point(0, 1), new Key(new Maze.Point(0, 1), ColorableTile.Color.Blue));
 
-        try{ Maze.player.move(Direction.Right); }
-        catch(IllegalArgumentException exception){ assert Maze.player.getPos().equals(new Maze.Point(0, 0)); }
+        try {
+            Maze.player.move(Direction.Right);
+        } catch (IllegalArgumentException exception) {
+            assert Maze.player.getPos().equals(new Maze.Point(0, 0));
+        }
         assert door.isObstructive();
 
         Maze.player.move(Direction.Down);
@@ -148,14 +151,17 @@ public class DomainTest{
      * a locked exit when the player has all the treasures.
      */
     @Test
-    public void unlockExitTest(){
+    public void unlockExitTest() {
         Maze.generateMap(new Maze.Point(5, 3), 1, -1);
         LockedExit exitGate = new LockedExit(new Maze.Point(1, 0));
         Maze.setTile(new Maze.Point(1, 0), exitGate);
         Maze.setTile(new Maze.Point(0, 1), new Treasure(new Maze.Point(0, 1)));
 
-        try{ Maze.player.move(Direction.Right); }
-        catch(IllegalArgumentException exception){ assert Maze.player.getPos().equals(new Maze.Point(0, 0)); }
+        try {
+            Maze.player.move(Direction.Right);
+        } catch (IllegalArgumentException exception) {
+            assert Maze.player.getPos().equals(new Maze.Point(0, 0));
+        }
         assert exitGate.isObstructive();
         assert Maze.getTreasuresLeft() == 1;
 
@@ -168,13 +174,13 @@ public class DomainTest{
     }
 
     /**
-     * Checks that the player moves 2 tiles if 
+     * Checks that the player moves 2 tiles if
      * they step on a bounce pad. Also checks that
      * a deletedtile will not still
      * move the player.
      */
     @Test
-    public void bouncePadTest(){
+    public void bouncePadTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         BouncyPad pad = new BouncyPad(new Maze.Point(2, 0), Direction.Right);
         Maze.setTile(new Maze.Point(1, 0), new BouncyPad(new Maze.Point(1, 0), Direction.Right));
@@ -190,11 +196,11 @@ public class DomainTest{
     }
 
     /**
-     * Checks that the enemy can kill the player 
+     * Checks that the enemy can kill the player
      * and move properly.
      */
     @Test
-    public void enemyTest(){
+    public void enemyTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         GummyGuard enemy = new GummyGuard(new Maze.Point(3, 0), Direction.Left);
         Maze.entities.add(enemy);
@@ -222,13 +228,13 @@ public class DomainTest{
     }
 
     /**
-     * Checks that the player loses the game if 
+     * Checks that the player loses the game if
      * they step on a milk tile. Also checks that
      * a deleted milk tile will not still
      * kill the player.
      */
     @Test
-    public void playerDeathTest(){
+    public void playerDeathTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         Maze.setTile(new Maze.Point(1, 0), new MilkPuddle(new Maze.Point(1, 0)));
         Maze.setTile(new Maze.Point(2, 0), new MilkPuddle(new Maze.Point(2, 0)));
@@ -245,22 +251,22 @@ public class DomainTest{
      * Checks that the player can walk onto the exit.
      */
     @Test
-    public void exitLevelTest(){
+    public void exitLevelTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         Maze.setTile(new Maze.Point(1, 0), new Exit(new Maze.Point(1, 0)));
 
         Maze.player.move(Direction.Right);
-        
+
         assert Maze.getNextLevel() == -1;
         assert Maze.gameComplete();
         assert Maze.gameWon();
     }
 
-    /** 
+    /**
      * Checks that all actions undo properly.
      */
     @Test
-    public void undoTest(){
+    public void undoTest() {
         Maze.generateMap(new Maze.Point(5, 5), 1, -1);
         Maze.player.setPos(new Maze.Point(1, 1));
         Maze.setTile(new Maze.Point(2, 1), new Key(new Maze.Point(2, 1), ColorableTile.Color.Blue));
@@ -297,46 +303,46 @@ public class DomainTest{
         // Shouldn't do anything, as player doesn't have observers attached to other entities.
         Maze.player.deleteEntity();
 
-        while(!actions.empty()){
+        while (!actions.empty()) {
             Maze.undo(List.of(actions.pop()));
         }
         assert Maze.player.getPos().equals(new Maze.Point(1, 1));
         assert Maze.getStringState().equals(
-            "GGGGG\n" +
-            "GGKGG\n" + 
-            "GDTGG\n" +
-            "GLGGG\n" +
-            "GGGGG\n" 
-        ): Maze.getStringState();
+                "GGGGG\n" +
+                        "GGKGG\n" +
+                        "GDTGG\n" +
+                        "GLGGG\n" +
+                        "GGGGG\n"
+        ) : Maze.getStringState();
     }
 
-    /** 
+    /**
      * Checks that an action can be redone properly.
      */
-    @Test 
-    public void redoTest(){
+    @Test
+    public void redoTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
 
         Maze.player.moveAndTurn(Direction.Right);
         assert Maze.player.hasAction();
         Entity.Action action = Maze.getChangeMap().get(0);
-        
+
         Maze.undo(List.of(action));
         Maze.apply(List.of(action));
         assert Maze.player.getPos().equals(new Maze.Point(1, 0));
         assert Maze.getStringState().equals(
-            "GGGGG\n" +
-            "GGGGG\n" + 
-            "GGGGG\n"
-        ): Maze.getStringState();
+                "GGGGG\n" +
+                        "GGGGG\n" +
+                        "GGGGG\n"
+        ) : Maze.getStringState();
     }
 
     /**
-     * Makes sure the Direction enum has 
+     * Makes sure the Direction enum has
      * the correct opposite directions
      */
-    @Test 
-    public void oppositeDirectionsTest(){
+    @Test
+    public void oppositeDirectionsTest() {
         assert Direction.Up.opposite() == Direction.Down;
         assert Direction.Down.opposite() == Direction.Up;
         assert Direction.Left.opposite() == Direction.Right;
@@ -348,7 +354,7 @@ public class DomainTest{
      * create tiles based on the ID.
      */
     @Test
-    public void successfulFactoryTest(){
+    public void successfulFactoryTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         Tile wall = TileDatabase.create("wall", new Maze.Point(1, 0));
         InfoField infoTile = (InfoField) TileDatabase.create("info", new Maze.Point(1, 0), "Hello");
@@ -363,24 +369,30 @@ public class DomainTest{
      * are given.
      */
     @Test
-    public void incorrectParametersTest(){
+    public void incorrectParametersTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
-        try{ TileDatabase.create("wall", "Wrong type here"); }
-        catch(IllegalArgumentException e){ return; }
+        try {
+            TileDatabase.create("wall", "Wrong type here");
+        } catch (IllegalArgumentException e) {
+            return;
+        }
         assert false;
     }
 
     /**
      * Checks that the tile factory method will throw
-     * an IllegalStateException if the constructor of a class throws 
-     * an error. In this case it is done by providing a point outside of the 
+     * an IllegalStateException if the constructor of a class throws
+     * an error. In this case it is done by providing a point outside of the
      * tileMap bounds.
      */
     @Test
-    public void unsuccessfulFactoryTest(){
+    public void unsuccessfulFactoryTest() {
         Maze.generateMap(new Maze.Point(1, 1), 0, -1);
-        try{ TileDatabase.create("wall", new Maze.Point(1, 0)); }
-        catch(IllegalStateException e){ return; }
+        try {
+            TileDatabase.create("wall", new Maze.Point(1, 0));
+        } catch (IllegalStateException e) {
+            return;
+        }
         assert false;
     }
 
@@ -389,7 +401,7 @@ public class DomainTest{
      * when using the getID() methods.
      */
     @Test
-    public void getIDTest(){
+    public void getIDTest() {
         Maze.generateMap(new Maze.Point(5, 3), 0, -1);
         Tile wall = TileDatabase.create("wall", new Maze.Point(1, 0));
 
@@ -399,10 +411,10 @@ public class DomainTest{
 
     /**
      * Making sure equals() and hashCode() in
-     * Maze.Point is reflexive, transitive, and symmetric. 
+     * Maze.Point is reflexive, transitive, and symmetric.
      */
     @Test
-    public void pointTest(){
+    public void pointTest() {
         Maze.Point pointOne = new Maze.Point(5, 5);
         Maze.Point pointTwo = new Maze.Point(1, 5).add(5, 0).subtract(1, 0);
 
