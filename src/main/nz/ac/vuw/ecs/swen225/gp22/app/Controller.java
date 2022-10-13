56dc533a6b9f3b269785b7fa00afcbcd5daccf70
Molly Henry <henrymoll@ecs.vuw.ac.nz>
2022-10-13 21:02:43 +1300

    Cleaned Controller class

--- src/main/nz/ac/vuw/ecs/swen225/gp22/app/Controller.java
+++ src/main/nz/ac/vuw/ecs/swen225/gp22/app/Controller.java
@@ -9,91 +9,95 @@ import java.awt.event.KeyEvent;
  * Controller that connects key actions to actions
  *
  * @author Molly Henry, 300562038
- * @version 1.2
+ * @version 1.4
  */
 public class Controller extends Keys {
-    private final Base b;
+	private final Base b; //current Base, to call actions on
 
-    /**
-     * Menu commands, exiting, replaying, making new levels
-     *
-     * @param b Base that actions happen
-     */
-    public Controller(Base b) {
-        this.b = b;
-        constantCommand();
-    }
+	/**
+	 * Menu commands, exiting, replaying, making new levels
+	 *
+	 * @param b Base that actions happen
+	 */
+	public Controller(Base b) {
+		this.b = b;
+		constantCommand();
+	}
 
-    /**
-     * Controller for when game is running
-     *
-     * @param b      base which commands get run on
-     * @param paused is the game paused?
-     */
-    public Controller(Base b, boolean paused) {
-        this.b = b;
-        constantCommand();
-        setAction(KeyEvent.VK_S, b::saveExit, true); //ctrl s
-        setAction(KeyEvent.VK_Q, b::saveGame, true); //ctrl q
+	/**
+	 * Controller for when game is running.
+	 *
+	 * @param b      base which commands get run on
+	 * @param paused is the game paused?
+	 */
+	public Controller(Base b, boolean paused) {
+		this.b = b;
+		constantCommand();
+		setAction(KeyEvent.VK_S, b::saveExit, true); //ctrl s
+		setAction(KeyEvent.VK_Q, b::saveGame, true); //ctrl q
 
-        if (paused) {
-            setAction(KeyEvent.VK_ESCAPE, b::unPause, false); //esc
-        } else {
-            setAction(KeyEvent.VK_SPACE, b::pause, false); //space
+		if (paused) {
+			setAction(KeyEvent.VK_ESCAPE, b::unPause, false); //esc
+		} else {
+			setAction(KeyEvent.VK_SPACE, b::pause, false); //space
 
-            setAction(KeyEvent.VK_UP, () -> b.movePlayer(Entity.Direction.Up));
-            setAction(KeyEvent.VK_DOWN, () -> b.movePlayer(Entity.Direction.Down));
-            setAction(KeyEvent.VK_LEFT, () -> b.movePlayer(Entity.Direction.Left));
-            setAction(KeyEvent.VK_RIGHT, () -> b.movePlayer(Entity.Direction.Right));
-        }
-    }
+			setAction(KeyEvent.VK_UP, () -> b.movePlayer(Entity.Direction.Up));
+			setAction(KeyEvent.VK_DOWN, () -> b.movePlayer(Entity.Direction.Down));
+			setAction(KeyEvent.VK_LEFT, () -> b.movePlayer(Entity.Direction.Left));
+			setAction(KeyEvent.VK_RIGHT, () -> b.movePlayer(Entity.Direction.Right));
+		}
+	}
 
-    /**
-     * Controller for Pop-up window
-     * @param b
-     * @param dialog
-     */
-    public Controller(Base b, JDialog dialog) {
-        this.b = b;
+	/**
+	 * Controller for Pop-up window.
+	 *
+	 * @param b      current base
+	 * @param dialog current pop-up
+	 */
+	public Controller(Base b, JDialog dialog) {
+		this.b = b;
 
-        setAction(KeyEvent.VK_X, () -> {
-            dialog.setVisible(false);
-            b.exitGame();
-        }, true); //ctrl X
+		setAction(KeyEvent.VK_X, () -> {
+			dialog.setVisible(false);
+			b.exitGame();
+		}, true); //ctrl X
 
-        setAction(KeyEvent.VK_R, () -> {
-            dialog.setVisible(false);
-            b.loadGame();
-        }, true); //ctrl r
+		setAction(KeyEvent.VK_R, () -> {
+			dialog.setVisible(false);
+			b.loadGame();
+		}, true); //ctrl r
 
-        setAction(KeyEvent.VK_1, () -> {
-            dialog.setVisible(false);
-            b.newGame(1);
-        }, true); //ctrl 1
+		setAction(KeyEvent.VK_1, () -> {
+			dialog.setVisible(false);
+			b.newGame(1);
+		}, true); //ctrl 1
 
-        setAction(KeyEvent.VK_2, () -> {
-            dialog.setVisible(false);
-            b.newGame(2);
-        }, true); //ctrl 2
+		setAction(KeyEvent.VK_2, () -> {
+			dialog.setVisible(false);
+			b.newGame(2);
+		}, true); //ctrl 2
 
-        setAction(KeyEvent.VK_S, b::saveExit, true); //ctrl s
+		setAction(KeyEvent.VK_S, b::saveExit, true); //ctrl s
 
-        setAction(KeyEvent.VK_Q, b::saveGame, true); //ctrl q
+		setAction(KeyEvent.VK_Q, b::saveGame, true); //ctrl q
 
-        setAction(KeyEvent.VK_ESCAPE, () -> {
-            dialog.setVisible(false);
-            b.resetFocus();
-            b.unPause();
-        }, false); //esc
-    }
+		setAction(KeyEvent.VK_ESCAPE, () -> {
+			dialog.setVisible(false);
+			b.resetFocus();
+			b.unPause();
+		}, false); //esc
+	}
 
 
-    private void constantCommand() {
-        setAction(KeyEvent.VK_X, b::exitGame, true); //ctrl X
-        setAction(KeyEvent.VK_R, b::loadGame, true); //ctrl r
-        setAction(KeyEvent.VK_1, () -> b.newGame(1), true); //ctrl 1
-        setAction(KeyEvent.VK_2, () -> b.newGame(2), true); //ctrl 2
+	/**
+	 * Actions that exist in most windows.
+	 */
+	private void constantCommand() {
+		setAction(KeyEvent.VK_X, b::exitGame, true); //ctrl X
+		setAction(KeyEvent.VK_R, b::loadGame, true); //ctrl r
+		setAction(KeyEvent.VK_1, () -> b.newGame(1), true); //ctrl 1
+		setAction(KeyEvent.VK_2, () -> b.newGame(2), true); //ctrl 2
 
-    }
+	}
 
 }
